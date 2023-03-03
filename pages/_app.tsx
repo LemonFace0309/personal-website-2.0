cf171fb630b3435a209022fe389307e7bd6e4691
Charles Liu <charles.liu0309@gmail.com>
2023-03-03 04:56:55 -0500

    updated seo keywords

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,24 @@ import { ThemeProvider } from 'next-themes';
 
 import SEO from '../next-seo.config';
 
+const keywords = [
+  'Charles',
+  'Charles Liu',
+  'University of Waterloo',
+  'Software Developer',
+  'CS',
+  'Computer Science',
+  'CertiK',
+  'On Deck',
+  'Student',
+];
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <DefaultSeo {...SEO} />
       <Head>
+        <meta name="keywords" content={keywords.join(', ')} />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Layout>
